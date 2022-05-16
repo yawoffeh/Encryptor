f67907da5b2a09794ec6a908951d5334d005d5a5
*Red <82349700+yawoffeh@users.noreply.github.com>
2022-05-16 07:20:02 +0000

    Update Encryptor

--- Encryptor.py
+++ Encryptor.py
@@ -4,14 +4,14 @@ import os
 files = os.listdir() #get files in directory
 
 def main():
-	passw = "solstice" #password
-	bufferSize = 64 * 1024 
-	for file in files:
-		try:
-			pyAesCrypt.encryptFile(file, file + ".out", passw, bufferSize)
-			print("Encrypted")
-		except:
-			print("unsucessful")
+    passw = "solstice" #password
+    bufferSize = 64 * 1024 
+        for file in files:
+	    try:
+		pyAesCrypt.encryptFile(file, file + ".out", passw, bufferSize)
+		print("Encrypted")
+	    except:
+		print("unsucessful")
 
 if __name__ == "__main__":
 	main()
